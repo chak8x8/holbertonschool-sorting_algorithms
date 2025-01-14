cac81805ea913de78ee1805e533d98980fd5e5f3
root <chak8x8@gmail.com>
2025-01-14 14:55:31 +0000

    sort.h

--- sort.h
+++ sort.h
@@ -19,6 +19,7 @@ typedef struct listint_s
 } listint_t;
 
 void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
 
 /* Function prototypes */
 void bubble_sort(int *array, size_t size);
